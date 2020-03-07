da8b1ab68c83a7c40689dabf2e3e97f28fdc0ce2
Praveen Srinivasan <popapa@gmail.com>
2020-03-07 14:49:22 -0800

    Got google test working.

--- gradlew.bat
+++ gradlew.bat
@@ -1,21 +1,3 @@
-@rem ################################################################################
-@rem #  Licensed to the Apache Software Foundation (ASF) under one
-@rem #  or more contributor license agreements.  See the NOTICE file
-@rem #  distributed with this work for additional information
-@rem #  regarding copyright ownership.  The ASF licenses this file
-@rem #  to you under the Apache License, Version 2.0 (the
-@rem #  "License"); you may not use this file except in compliance
-@rem #  with the License.  You may obtain a copy of the License at
-@rem #
-@rem #      http://www.apache.org/licenses/LICENSE-2.0
-@rem #
-@rem #  Unless required by applicable law or agreed to in writing, software
-@rem #  distributed under the License is distributed on an "AS IS" BASIS,
-@rem #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-@rem #  See the License for the specific language governing permissions and
-@rem # limitations under the License.
-@rem ################################################################################
-
 @if "%DEBUG%" == "" @echo off
 @rem ##########################################################################
 @rem
@@ -32,7 +14,7 @@ set APP_BASE_NAME=%~n0
 set APP_HOME=%DIRNAME%
 
 @rem Add default JVM options here. You can also use JAVA_OPTS and GRADLE_OPTS to pass JVM options to this script.
-set DEFAULT_JVM_OPTS=
+set DEFAULT_JVM_OPTS="-Xmx64m"
 
 @rem Find java.exe
 if defined JAVA_HOME goto findJavaFromJavaHome
